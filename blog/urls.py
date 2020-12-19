127e861071d083413231b0f3e8ac16187866c576
Kirill Sobolev <smail.ytn@gmail.com>
2020-12-20 00:13:49 +0300

    model post

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import *
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
-    path('', posts_list, name='posts_lists_url'),
-    path('post/<str:slug>/', post_detail, name='post_detail_url')
+    path('', posts_list, name='posts_list_url'),
+    path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -3,10 +3,10 @@ from .models import *
 from .utils import ObjectDetailMixin
 from django.views.generic import View
 
-def estates_list(request):
-    estates = Post.objects.all()
+def posts_list(request):
+    posts = Post.objects.all()
     return render(request, 'blog/posts_list.html', context={'posts': posts})
 
-class EstateDetail(ObjectDetailMixin, View):
+class PostDetail(ObjectDetailMixin, View):
     model = Post
     template = 'blog/post_detail.html'

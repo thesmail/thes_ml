24633b18fecb074f771ec2eaefa00527ade6a9e5
Kirill Sobolev <smail.ytn@gmail.com>
2021-10-02 19:12:03 +0300

    add sitemaps

--- thes_ml/settings.py
+++ thes_ml/settings.py
@@ -25,6 +25,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'blog',
     'django_summernote',
 ]
--- thes_ml/urls.py
+++ thes_ml/urls.py
@@ -1,11 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
+from blog.sitemaps import PostSitemap
+
+sitemaps = {
+    'posts': PostSitemap,
+}
 
 urlpatterns = [
     path('golem/', admin.site.urls),
     path('', include('blog.urls')),
     path('', include('pagedown.urls')),
     path('summernote/', include('django_summernote.urls')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]
 
 handler404 = 'blog.views.error_404'
